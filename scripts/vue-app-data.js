99f891fc1ad782c7076e9e1d81c25df12ee2572b
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-12-20 00:25:47 +0800

    Tue Dec 20 12:25:47 AM CST 2022

--- scripts/vue-app-data.js
+++ scripts/vue-app-data.js
@@ -26,9 +26,97 @@ if (!line) {
   line = 'true'
 }
 
+String.prototype.hashCode = function() {
+  var hash = 0,
+    i, chr;
+  if (this.length === 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    chr = this.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+let colors = {
+  'sticky': {
+    font: '#000',
+    background: '#feff9c',
+    border: 'rgba(0,0,0,0.5)'
+  },
+  'white': {
+    font: '#000',
+    background: '#fff',
+    border: 'rgba(0,0,0,0.5)'
+  },
+  'red': {
+    font: '#db2828',
+    background: '#ffe8e6',
+    border: '#db2828'
+  },
+  'orange': {
+    font: '#f2711c',
+    background: '#ffedde',
+    border: '#f2711c'
+  },
+  'yellow': {
+    font: '#b58105',
+    background: '#fff8db',
+    border: '#b58105'
+  },
+  'olive': {
+    font: '#8abc1e',
+    background: '#fbfdef',
+    border: '#8abc1e'
+  },
+  'teal': {
+    font: '#10a3a3',
+    background: '#e1f7f7',
+    border: '#10a3a3'
+  },
+  'blue': {
+    font: '#2185d0',
+    background: '#dff0ff',
+    border: '#2185d0'
+  },
+  'violet': {
+    font: '#6435c9',
+    background: '#eae7ff',
+    border: '#6435c9'
+  },
+  'purple': {
+    font: '#a333c8',
+    background: '#f6e7ff',
+    border: '#a333c8'
+  },
+  'pink': {
+    font: '#e03997',
+    background: '#ffe3fb',
+    border: '#e03997'
+  },
+  'brown': {
+    font: '#a5673f',
+    background: '#f1e2d3',
+    border: '#a5673f'
+  },
+  'black': {
+    font: 'rgba(255,255,255,.9)',
+    background: '#1b1c1d',
+    border: '#1b1c1d'
+  }
+}
+
 let color = urlParams.get('color')
 if (!color) {
-  color = 'sticky'
+  if (id) {
+    let idCode = id.hashCode()
+    let colorNames = Object.keys(colors)
+    let colorIndex = idCode % colorNames.length
+    color = colorNames[colorIndex]
+  }
+  else {
+    color = 'sticky'
+  }
 }
 // console.log(color)
 // console.log(line)
@@ -44,73 +132,7 @@ var appData = {
   cacheKey,
   inited: false,
   color,
-  colors: {
-    'sticky': {
-      font: '#000',
-      background: '#feff9c',
-      border: 'rgba(0,0,0,0.5)'
-    },
-    'white': {
-      font: '#000',
-      background: '#fff',
-      border: 'rgba(0,0,0,0.5)'
-    },
-    'red': {
-      font: '#db2828',
-      background: '#ffe8e6',
-      border: '#db2828'
-    },
-    'orange': {
-      font: '#f2711c',
-      background: '#ffedde',
-      border: '#f2711c'
-    },
-    'yellow': {
-      font: '#b58105',
-      background: '#fff8db',
-      border: '#b58105'
-    },
-    'olive': {
-      font: '#8abc1e',
-      background: '#fbfdef',
-      border: '#8abc1e'
-    },
-    'teal': {
-      font: '#10a3a3',
-      background: '#e1f7f7',
-      border: '#10a3a3'
-    },
-    'blue': {
-      font: '#2185d0',
-      background: '#dff0ff',
-      border: '#2185d0'
-    },
-    'violet': {
-      font: '#6435c9',
-      background: '#eae7ff',
-      border: '#6435c9'
-    },
-    'purple': {
-      font: '#a333c8',
-      background: '#f6e7ff',
-      border: '#a333c8'
-    },
-    'pink': {
-      font: '#e03997',
-      background: '#ffe3fb',
-      border: '#e03997'
-    },
-    'brown': {
-      font: '#a5673f',
-      background: '#f1e2d3',
-      border: '#a5673f'
-    },
-    'black': {
-      font: 'rgba(255,255,255,.9)',
-      background: '#1b1c1d',
-      border: '#1b1c1d'
-    }
-  }
+  colors
 }
 
 // console.log(LOCAL_CONFIG)
